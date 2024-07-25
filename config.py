2a9d216480ef5e17564cc7ffb61e1adb63331ac6
Oliinyk Maksym <maxim@oliinyk.com.ua>
2024-07-25 08:18:43 -0400

    Updated resume

--- config.py
+++ config.py
@@ -17,9 +17,22 @@ SOCIAL_MEDIA = {
     "GitHub": "https://github.com/oliinykm99",
     "X (Twitter)": "https://x.com/maximolii",
 }
+
 PROJECTS = {
-    "🏆 Dashboards - DeFi protocols featuring various metrics (Aave, Liquity)": "",
-    "🏆 GMX V1 Dashboard - DeFi protocol eaturing various metrics (Aave, Liquity)": "https://github.com/oliinykm99/gmx-st-dashboard",
-    "🏆 ETH Price XGBoost Prediction - Prediction Ethereum Price with Gradient-Boosted Decision Tree": "https://github.com/oliinykm99/ETH-XGBoost-prediction",
-    "🏆 Dashboards - DeFi protocols featuring various metrics (Aave, Liquity)": "",
+    "✅ Dashboards - DeFi protocols featuring various metrics (Aave, Liquity)": "",
+    "✅ GMX V1 Dashboard - DeFi protocol featuring various metrics": "https://github.com/oliinykm99/gmx-st-dashboard",
+    "✅ ETH Price Prediction - Predicting Ethereum using Gradient Boosted Decision Trees": "https://github.com/oliinykm99/ETH-XGBoost-prediction",
+    "✅ Dashboards - DeFi protocols featuring various metrics (Aave, Liquity)": "",
+}
+
+CERTIFICATIONS = {
+    "Databases and SQL for Data Science with Python (20 hours)": "https://www.coursera.org/account/accomplishments/verify/WZDN8F96D2ST",
+    "Advance Your Python Skills for Data Science (20 hours)": "https://www.linkedin.com/learning/certificates/72deb536fe55d9358a0a7c4af06ce4d99f7cf79a85c109e6946ffab001616cf8?trk=backfilled_certificate",
+    "Data Analysis with Python (300 hours)": "https://www.freecodecamp.org/certification/oliinykm/data-analysis-with-python-v7",
+}
+
+ICONS = {
+    "IBM": "https://static-00.iconduck.com/assets.00/ibm-icon-2048x820-8w0dzwvm.png",
+    "LinkedIn": "https://asset.brandfetch.io/idJFz6sAsl/idykwa8_aj.svg?updated=1668071026733",
+    "freeCodeCamp": "https://www.freecodecamp.org/favicon-32x32.png?v=6cba562cbd10e31af925a976f3db73f7",
 }
